544daa38879603d257a92e33755bf548b89284c1
FevenSeyfu <fevenseyfu00@gmail.com>
2024-03-27 13:46:32 +0300

    Complete form validation and show success message for valid input format

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,49 @@
-import './style.css'
+import "./style.css";
+let email: string | undefined;
+const container = document.getElementsByClassName("container")[0];
+const signupForm = document.getElementById("signup-form");
+const signupButton = signupForm?.getElementsByTagName("button")[0];
+const signupFormInput = signupForm?.getElementsByTagName("input")[0];
+const signupFormError = signupForm?.getElementsByClassName("error-message")[0];
+// success message
+const successMessage = document.getElementById("success-message");
+const inputEmail = successMessage?.getElementsByClassName("registered-email")[0];
+const successMessageDismissButton =
+  successMessage?.getElementsByTagName("button")[0];
+
+// form validation, check email format and display success message or show error message
+const validateEmail = (email: string) => {
+  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  return emailPattern.test(email);
+};
+const handleInvalidEmail = () => {
+  signupButton?.setAttribute("disabled", "true");
+  signupFormError?.classList.remove("hidden");
+  signupFormInput?.classList.add("error");
+};
+const handleValidEmail = () => {
+  email = signupFormInput?.value;
+  signupButton?.removeAttribute("disabled");
+  signupFormError?.classList.add("hidden");
+  signupFormInput?.classList.remove("error");
+};
+signupFormInput?.addEventListener("input", (e: Event) => {
+    validateEmail((e.target as HTMLInputElement).value)
+        ? handleValidEmail()
+        : handleInvalidEmail();
+});
+
+signupButton?.addEventListener("click", (e: Event) => {
+    e.preventDefault();
+    inputEmail && (inputEmail.textContent = email || '');
+    signupForm?.classList.add("hidden");
+    successMessage?.classList.remove("hidden");
+    container?.classList.add("success");
+});
+
+successMessageDismissButton?.addEventListener("click", () => {
+    successMessage?.classList.add("hidden");
+    signupForm?.classList.remove("hidden");
+    container?.classList.remove("success");
+    signupFormInput && (signupFormInput.value = '');
+});
